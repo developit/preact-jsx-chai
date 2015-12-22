add6a83eb6a88785ab273dd31e20e40c052354d8
Jason Miller <jason@developit.ca>
2015-12-21 19:05:00 -0500

    Preliminary tests

---
+++ test/index.js
@@ -0,0 +1,43 @@
+import { default as assertJsx, options } from '../src';
+import { h } from 'preact';
+import { expect, default as chai } from 'chai';
+chai.use(assertJsx);
+/**@jsx h */
+
+/*eslint-env mocha */
+/*eslint max-nested-callbacks:0*/
+
+describe('preact-jsx-chai', () => {
+	describe('assertJsx()', () => {
+		it('should be a function', () => {
+			expect(assertJsx).to.be.a('function');
+		});
+
+		it('should be a function', () => {
+			expect(assertJsx).to.be.a('function');
+		});
+
+		describe('sanity', () => {
+			it('be triggered when JSX is tested', () => {
+				expect(<jsx />).to.deep.equal(<jsx />);
+			});
+
+			it('be triggered when JSX is tested', () => {
+				expect(<jsx />).to.deep.equal({
+					nodeName: 'jsx',
+					attributes: {}
+				});
+			});
+		});
+	});
+
+	describe('options', () => {
+		it('should be an object', () => {
+			expect(options).to.be.an('object');
+		});
+
+		xit('should support isJsx()', () => {
+			// @TODO
+		});
+	});
+});
